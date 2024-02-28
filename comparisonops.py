e2884a6213b0a57c576515922dbcdf111dea4d75
dettod2k <dettod2k@gmail.com>
2024-02-28 11:45:24 +0200

    switched to comments

--- comparisonops.py
+++ comparisonops.py
@@ -7,18 +7,17 @@
 #variable. And you can use the equality operator to compare various
 #combinations. All of the following are legal first lines in if statements:
 
-if full_name == "Mark" + " " + "Myers":
-if full_name == first_name + " " + "Myers":
-if full_name == first_name + " " + last_name:
-if total_cost == 81.50 + 135:
-if total_cost == materials_cost + 135:
-if total_cost == materials_cost + labor_cost:
-if x + y == a - b:
+# if full_name == "Mark" + " " + "Myers":
+# if full_name == first_name + " " + "Myers":
+# if full_name == first_name + " " + last_name:
+# if total_cost == 81.50 + 135:
+# if total_cost == materials_cost + 135:
+# # if x + y == a - b:
 
 # Another comparison operator, !=, is the opposite of ==. It means is not
 # equal to.
-1 if your_ticket_number != 487208:
-2 print("Better luck next time.")
+# 1 if your_ticket_number != 487208:
+# 2 print("Better luck next time.")
 
 # Like ==, the not-equal operator can be used to compare numbers, strings,
 # variables, math expressions, and combinations.
